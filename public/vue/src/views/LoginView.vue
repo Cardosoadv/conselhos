b1a72cefa64daf74dafce15540ea91063eec8331
Cardosoadv <fabianocardoso.adv@gmail.com>
2023-12-09 09:45:49 -0300

    criada tela de registro e login

---
+++ public/vue/src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import navbarView from '../components/navbarView.vue'
+import sidebarView from '../components/sidebarView.vue'
+import loginView from '../components/forms/loginView.vue'
+import footerView from '../components/footerView.vue'
+
+</script>
+
+<template>
+     <div class="app-wrapper"><!--begin::Header-->
+      <navbarView />
+
+      <sidebarView />
+
+      <loginView/>
+
+      <footerView />
+    </div>
+
+</template>
